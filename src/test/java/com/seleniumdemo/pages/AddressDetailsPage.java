4719a0f4225d6145db2ce9c048bb61c70d44b3f1
Patryk <trzodiii@gmail.com>
2024-02-16 20:50:45 +0100

    Add customer details method

--- src/test/java/com/seleniumdemo/pages/AddressDetailsPage.java
+++ src/test/java/com/seleniumdemo/pages/AddressDetailsPage.java
@@ -1,9 +1,12 @@
 package com.seleniumdemo.pages;
 
+import com.seleniumdemo.models.Customer;
+import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class AddressDetailsPage {
 
@@ -16,7 +19,7 @@ public class AddressDetailsPage {
     @FindBy(id = "billing_company")
     private WebElement conmapnyNameInput;
 
-    @FindBy(id = "select2-billing_country-container")
+    @FindBy(id = "billing_country")
     private WebElement billingCountrySelect;
 
     @FindBy(id = "billing_address_1")
@@ -26,7 +29,7 @@ public class AddressDetailsPage {
     private WebElement billingAddressInputOptional;
 
     @FindBy(id = "billing_postcode")
-    private WebElement postcodeInput;
+    private WebElement billingPostcodeInput;
 
     @FindBy(id = "billing_city")
     private WebElement cityInput;
@@ -45,9 +48,27 @@ public class AddressDetailsPage {
     private WebDriver driver;
 
     public AddressDetailsPage(WebDriver driver) {
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
+    public OrderDetailsPage     fillCustomerDetails(Customer customer, String comments) throws InterruptedException {
+        firstNameInput.sendKeys(customer.getFirstName());
+        lastNameInput.sendKeys(customer.getLastName());
+        conmapnyNameInput.sendKeys(customer.getCompanyName());
+        Select contrySelect = new Select(billingCountrySelect);
+        contrySelect.selectByVisibleText(customer.getCountry());
+        billingAddressInput.sendKeys(String.format("%s %s", customer.getStreet(), customer.getFlatNumber()));
+        billingPostcodeInput.sendKeys(customer.getZipCode());
+        cityInput.sendKeys(customer.getCity());
+        billingPhoneInput.sendKeys(customer.getPhone());
+        billingEmailInput.sendKeys(customer.getEmail());
+        commentsInput.sendKeys(comments);
+        Thread.sleep(1500);
+        placeOrderButton.click();
 
-}
+            return new OrderDetailsPage(driver);
+        }
+    }
+
+
